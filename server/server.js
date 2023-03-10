b44af9573461c11cccf5388fa8cfed90354794ca
Josh Cemine <j.jcemine@gmail.com>
2023-03-10 13:26:02 +0800

    Added DB to App connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const app = express();
 
+const mysql = require('mysql2');
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'password',
+    database: 'croma'
+});
+
+db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to database:', err);
+      return;
+    }
+    console.log('Connected to database.');
+  });
+
+
+db.query('SELECT * FROM users', (err, results) => {
+    if (err) {
+      console.error('Error querying database:', err);
+      return;
+    }
+    console.log('Query results:', results);
+  });
+  
+
 app.get("/api", (req, res) => {
     res.json({"users": ["userOne, userTwo, userThree, userFour"]})
 })
