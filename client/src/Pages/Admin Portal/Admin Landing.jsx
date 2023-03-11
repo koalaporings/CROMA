d50949e790d8ed958cd97883e1de698d021a4214
RosalesMiguel <m.rosales.mar123@gmail.com>
2023-03-11 11:07:59 +0800

    updated table

--- client/src/Pages/Admin Portal/Admin Landing.jsx
+++ client/src/Pages/Admin Portal/Admin Landing.jsx
@@ -3,11 +3,14 @@ import {useNavigate} from 'react-router-dom';
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
 import './Admin Landing.css';
-import { AiOutlineDoubleLeft, AiOutlineDoubleRight } from 'react-icons/ai'
-import { MdOutlineArrowBackIos, MdOutlineArrowForwardIos } from 'react-icons/md'
+import { AiOutlineDoubleLeft, AiOutlineDoubleRight } from 'react-icons/ai';
+import { MdOutlineArrowBackIos, MdOutlineArrowForwardIos } from 'react-icons/md';
+import { BsExclamationLg, BsChevronDown } from 'react-icons/bs';
+import { RiArrowDropDownLine } from 'react-icons/ri';
+
+
 
 const AdminLanding = ({children}) => {
-
     return(
         <div>
             <Header/>
@@ -16,7 +19,14 @@ const AdminLanding = ({children}) => {
                 <div className="table1-container">
                     <div className="table-top">
                         <div className="table-label">Transaction Requests</div>
-                        <div className="search-filter">Search Filter</div>
+                        <div className="search-filter">
+                            <div className='filter'>Filter by:</div>
+                            <div className="filter-dropdown">
+                                <div className="selected-filter-text">Date</div>
+                                <div className='dropdown-icon'><BsChevronDown/></div>
+                            </div>
+                            <input id="table1-input" className='search-box' type="text" name="search-input" placeholder='Search'/>   
+                        </div>
                     </div>
                     <table className="table1">
                         <tr className='header'>
@@ -26,87 +36,110 @@ const AdminLanding = ({children}) => {
                             <th className='transaction-request'>Transaction Request</th>
                             <th className='form'>Form</th>
                         </tr>
-                        <tr>
-                            <td></td>
-                            <td>10/01/2022</td>
-                            <td>Gabriel Howard Awatin</td>
-                            <td>True Copy of Grades</td>
-                            <td>
+                        <tr className='urgent-row'>
+                            <td className='urgent'>
+                                <div className="urgent-icon">
+                                    <BsExclamationLg/>
+                                </div>
+                                <div className="urgent-box">
+                                    URGENT
+                                </div>
+                            </td>
+                            <td className='date'>10/01/2022</td>
+                            <td className='student-name'>Gabriel Howard Awatin</td>
+                            <td className='transaction-request'>True Copy of Grades</td>
+                            <td className='form'>
                                 <button className='view'>
                                     View
                                 </button>
                             </td>
                         </tr>
                         <tr>
-                            <td></td>
-                            <td>10/01/2022</td>
-                            <td>John Oliver Ochea</td>
-                            <td>True Copy of Grades</td>
-                            <td>
+                            <td className='urgent'></td>
+                            <td className='date'>10/01/2022</td>
+                            <td className='student-name'>John Oliver Ochea</td>
+                            <td className='transaction-request'>True Copy of Grades</td>
+                            <td className='form'>
                                 <button className='view'>
                                     View
                                 </button>
                             </td>
                         </tr>
                         <tr>
-                            <td></td>
-                            <td>10/01/2022</td>
-                            <td>Gabriel Howard Awatin</td>
-                            <td>True Copy of Grades</td>
-                            <td>
+                            <td className='urgent'></td>
+                            <td className='date'>10/01/2022</td>
+                            <td className='student-name'>Gabriel Howard Awatin</td>
+                            <td className='transaction-request'>True Copy of Grades</td>
+                            <td className='form'>
                                 <button className='view'>
                                     View
                                 </button>
                             </td>
                         </tr>
                         <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <td className='urgent'></td>
+                            <td className='date'>10/01/2022</td>
+                            <td className='student-name'>Gabriel Howard Awatin</td>
+                            <td className='transaction-request'>True Copy of Grades</td>
+                            <td className='form'>
+                                <button className='view'>
+                                    View
+                                </button>
+                            </td>
                         </tr>
                         <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <td className='urgent'></td>
+                            <td className='date'>10/01/2022</td>
+                            <td className='student-name'>Gabriel Howard Awatin</td>
+                            <td className='transaction-request'>True Copy of Grades</td>
+                            <td className='form'>
+                                <button className='view'>
+                                    View
+                                </button>
+                            </td>
                         </tr>
                         <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <td className='urgent'></td>
+                            <td className='date'>10/01/2022</td>
+                            <td className='student-name'>Gabriel Howard Awatin</td>
+                            <td className='transaction-request'>True Copy of Grades</td>
+                            <td className='form'>
+                                <button className='view'>
+                                    View
+                                </button>
+                            </td>
                         </tr>
                         <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <td className='urgent'></td>
+                            <td className='date'>10/01/2022</td>
+                            <td className='student-name'>Gabriel Howard Awatin</td>
+                            <td className='transaction-request'>True Copy of Grades</td>
+                            <td className='form'>
+                                <button className='view'>
+                                    View
+                                </button>
+                            </td>
                         </tr>  
                         <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <td className='urgent'></td>
+                            <td className='date'></td>
+                            <td className='student-name'></td>
+                            <td className='transaction-request'></td>
+                            <td className='form'></td>
                         </tr>
                         <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <td className='urgent'></td>
+                            <td className='date'></td>
+                            <td className='student-name'></td>
+                            <td className='transaction-request'></td>
+                            <td className='form'></td>
                         </tr>  
                         <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <td className='urgent'></td>
+                            <td className='date'></td>
+                            <td className='student-name'></td>
+                            <td className='transaction-request'></td>
+                            <td className='form'></td>
                         </tr>                    
                     </table>
                     <div className="table1-footer">
@@ -118,7 +151,13 @@ const AdminLanding = ({children}) => {
                             <button className="table-page-button"><MdOutlineArrowForwardIos/></button>
                             <button className="table-page-button"><AiOutlineDoubleRight/></button>
                         </div>
-                        <div className="show-section">SHOW</div>
+                        <div className="show-section">
+                            <div className="show-text">SHOW</div>
+                            <div className="show-dropdown">
+                                <div className="show-number">1</div>
+                                <div className="show-icon"><RiArrowDropDownLine/></div>
+                            </div>
+                        </div>
                             
                     </div>
                 </div>
