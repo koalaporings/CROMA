4c3c333e85708831a25daf5aa9b7ba56bc8b2d9f
Gabriel <gabriel.awatin@module-zero.com>
2023-03-14 20:55:19 +0800

    added admin landing announcement table

--- client/src/Pages/Admin Portal/Admin Landing.jsx
+++ client/src/Pages/Admin Portal/Admin Landing.jsx
@@ -4,8 +4,8 @@ import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
 import TransactionTable from './Transaction Table';
 import OngoingTable from './Ongoing Table';
+import AnnouncementTable from './Announcement Table';
 import './Admin Landing.css';
-
 
 
 
@@ -15,15 +15,17 @@ const AdminLanding = ({children}) => {
             <div className="header-admin">
                 <Header/>
             </div>
+
             <div className='admin-container'>
-                <p>Placeholder text</p>
+                {/* <p>Placeholder text</p> */}
+                <AnnouncementTable/>
                 <TransactionTable/>
                 <OngoingTable/>
             </div>
+
             <div className="footer-admin">
                 <Footer/>
             </div>
-
         </div>
     )
 }
