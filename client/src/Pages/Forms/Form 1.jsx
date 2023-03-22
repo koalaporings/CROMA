6327ffbf29a5c48b23dcfb65f8ea4d075da62cdc
johnochea <johnoochea@gmail.com>
2023-03-22 16:19:44 +0800

    created interactive form request list (in student)

---
+++ client/src/Pages/Forms/Form 1.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Forms.css';
+
+
+const Form1 = ({children}) => {
+
+    return(
+        <div>
+            <span style={{fontSize: '40px'}}>
+                ***Insert True Copy of Grades (TCG) page here***
+            </span>
+        </div>
+    )
+}
+
+
+
+export default Form1;
---
+++ client/src/Pages/Forms/Form 2.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Forms.css';
+
+
+const Form2 = ({children}) => {
+
+    return(
+        <div>
+            <span style={{fontSize: '40px'}}>
+                ***Insert Form #2 page here***
+            </span>
+        </div>
+    )
+}
+
+
+
+export default Form2;
