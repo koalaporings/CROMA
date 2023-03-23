a8f6bbfdcc08edc894c6fb4e5d337454884a6ff7
Gabriel <gabriel.awatin@module-zero.com>
2023-03-24 01:09:38 +0800

    major fixes in announcement page

--- client/src/Pages/Announcements/Announcements.jsx
+++ client/src/Pages/Announcements/Announcements.jsx
@@ -4,6 +4,8 @@ import Footer from '../../Components/Footer/Footer';
 import Header from '../../Components/Header/Header';
 import NavBar from '../../Components/Navigation Bar/NavBar';
 import Table from './Announcement Portal Table';
+import announcementIcon from '../../Assets/announcement-icon.svg';
+
 
 import './Announcements.css';
 
@@ -15,16 +17,23 @@ const AnnouncementPage = ({children}) => {
             <Header/>
 
             <div className='announce-container'>
+                <div className="header">
+                    <img alt="announcement-icon"
+                        src={announcementIcon}
+                        className="announcement-icon"/>
+                    <p className='announcement-text'>Announcement</p>
+                </div>
+
+                {/* <p>-</p>
                 <p>-</p>
                 <p>-</p>
                 <p>-</p>
-                <p>-</p>
-                <p>Announcements Page</p>
+                <p>Announcements Page</p> */}
                 <Table/>
 
             </div>
 
-            <Footer/>
+            <Footer/> 
         </div>
     )
 }
