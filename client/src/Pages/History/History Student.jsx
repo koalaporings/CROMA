0506e1cf7557107896ea09773eb62193b06378a0
Gabriel <gabriel.awatin@module-zero.com>
2023-04-07 21:22:22 +0800

    Create

--- client/src/Pages/History/History Student.jsx
+++ client/src/Pages/History/History Student.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
 import {useNavigate} from 'react-router-dom';
 import Footer from '../../Components/Footer/Footer';
 import Header from '../../Components/Header/Header';
 import NavBar from '../../Components/Navigation Bar/NavBar Student';
-
+import "./Modal.css";
+import Modal from './Modal.jsx';
 import './History.css';
+
+
+
 
 const StudentHistoryPage = ({children}) => {
 
+
+    const [isOpen, setIsOpen] = useState(false);
+
     return(
+
+        <main>
         <div>
             <NavBar/>
             <Header/>
@@ -21,8 +30,17 @@ const StudentHistoryPage = ({children}) => {
                 <p>Tracking History Student Page</p>
             </div>
 
+            <button className='action-button' onClick={() => setIsOpen(true)}>
+                View
+            </button>
+            {isOpen && <Modal setIsOpen={setIsOpen} />}
+
             {/* <Footer/> */}
         </div>
+
+        </main>
+
+        
     )
 }
 
---
+++ client/src/Pages/History/Modal.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Modal.css";
+import { RiCloseLine } from "react-icons/ri";
+
+const Modal = ({ setIsOpen }) => {
+  return (
+    <>
+      <div className="darkBG" onClick={() => setIsOpen(false)} />
+      <div className="centered">
+        <div className="view-modal">
+          <div className='view-modalHeader'>
+            <h5 className='view-heading'>True Copy of Grades</h5>
+          </div>
+          <button className="closeBtn" onClick={() => setIsOpen(false)}>
+            <RiCloseLine style={{ marginBottom: "-3px" }} />
+          </button>
+          <div className="view-modalContent">
+            CONTENT
+          </div>
+          <div className="view-modalActions">
+            <div className="view-modal-actionsContainer">
+              <button className="download-button" onClick={() => setIsOpen(false)}>
+                DOWNLOAD
+              </button>
+            </div>
+          </div> 
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Modal;
