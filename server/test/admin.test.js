1afce21704090c2631c03a054b55033007502c94
Josh Cemine <j.jcemine@gmail.com>
2023-05-22 17:21:04 +0800

    commit

--- server/test/admin.test.js
+++ server/test/admin.test.js
@@ -8,30 +8,6 @@ app.use(express.json());
 app.use('/', router);
 
 describe('API routes', () => {
-  describe('GET /approval_table', () => {
-    it('should return approval table data', async () => {
-      // Make a request to the /approval_table endpoint
-      const res = await request(app).get('/approval_table');
-
-      // Check for a successful response
-      expect(res.status).to.equal(200);
-      expect(res.body).to.be.an('array');
-      // Add additional checks for the response data structure and content as needed
-    });
-  });
-
-  describe('GET /ongoing_table', () => {
-    it('should return ongoing table data', async () => {
-      // Make a request to the /ongoing_table endpoint
-      const res = await request(app).get('/ongoing_table');
-
-      // Check for a successful response
-      expect(res.status).to.equal(200);
-      expect(res.body).to.be.an('array');
-      // Add additional checks for the response data structure and content as needed
-    });
-  });
-
   describe('PUT /transaction_status/:id', () => {
     it('should update transaction status and return the result', async () => {
       // Provide a valid transaction ID and status for testing
