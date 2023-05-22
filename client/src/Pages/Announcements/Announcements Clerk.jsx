59343c3f82376f0c63f7447ba6b292dfdb06046e
Gabriel <gabriel.awatin@module-zero.com>
2023-05-22 17:41:00 +0800

    Announcement Page for Clerk

--- client/src/Pages/Announcements/Announcements Clerk.jsx
+++ client/src/Pages/Announcements/Announcements Clerk.jsx
@@ -1,29 +1,62 @@
 import React from 'react';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 import Footer from '../../Components/Footer/Footer';
 import Header from '../../Components/Header/Header';
 import NavBar from '../../Components/Navigation Bar/NavBar Clerk';
+import AnnouncementTableComponent from '../../Components/Table/Announcement Table';
+import { useState, useEffect } from 'react';
+import announcementIcon from '../../Assets/announcement-icon.svg';
+
+
+import axios from 'axios'
 
 import './Announcements.css';
 
-const AnnouncementPage = ({children}) => {
+const AnnouncementClerkPage = ({children}) => {
+
+    const [announcementData, setAnnouncementData] = useState([]);
+
+
+    useEffect (() =>{
+        const fetchAllAnnouncement = async ()=>{
+            const response = await axios.get('http://localhost:5000/announcement_api/details')
+            setAnnouncementData(response.data)
+        }
+        fetchAllAnnouncement()
+        }, [])
+
+
+    console.log(announcementData)
+
 
     return(
         <div>
             <NavBar/>
             <Header/>
 
-            <div className='announce-container'>
-                <p>-</p>
-                <p>-</p>
-                <p>-</p>
-                <p>-</p>
-                <p>Announcements Page</p>
+            <div className='announcement-portal-container'>
+                <div className="announcement-portal-header">
+                    <img alt="announcement-portal-icon"
+                        src={announcementIcon}
+                        className="announcement-portal-icon"/>
+                    <p className='announcement-portal-text'>Announcement</p>
+                </div>
+                <div className="student-announcement-table-container">
+                    <AnnouncementTableComponent
+                        type = 'clerk_announcement_table'
+                        tableData = {announcementData}
+                    />
+                </div>
+
             </div>
 
-            <Footer/>
+            <div className='footer-admin-announcement'>
+            <Footer/> 
+            </div>
+
         </div>
     )
 }
 
-export default AnnouncementPage;
+export default AnnouncementClerkPage;
